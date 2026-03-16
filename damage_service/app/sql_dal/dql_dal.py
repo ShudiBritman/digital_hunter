9b33997a8141bdcf2f9a56ab213f14c095bf00fc
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-16 17:38:24 +0200

    fix table name

--- damage_service/app/sql_dal/dql_dal.py
+++ damage_service/app/sql_dal/dql_dal.py
@@ -6,7 +6,7 @@ conn = DataBase.get_connection()
 
 def update_table(data, table_name="damage"):
     cursor = conn.cursor()
-    query = '''INSERT INTO attack
+    query = f'''INSERT INTO {table_name}
         (attack_id, timestamp, entity_id, weapon_type)
         VALUES(%s, %s, %s, %s)'''
     cursor.execute(query, (
