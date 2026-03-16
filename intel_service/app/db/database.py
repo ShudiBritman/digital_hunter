bc8a41dce17cfe5800eafcf2a96ec10ee3d088f6
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-03-16 16:53:53 +0200

    add all tabels

--- intel_service/app/db/database.py
+++ intel_service/app/db/database.py
@@ -31,13 +31,33 @@ class DataBase:
             reported_lat FLOAT NOT NULL,
             reported_lon FLOAT NOT NULL,
             priority_level INT NOT NULL,
-            status VARCHAR(50) DEFAULT 'active');
+            status VARCHAR(50) DEFAULT 'active'
+            );
 
             CREATE TABLE IF NOT EXIST signals(
             timestamp DATETIME,
             signal_id INT PRIMARY KEY,
-            signal_type VARCHAR(50)
-            )
+            signal_type VARCHAR(50),
+            entity_id VARCHAR(50)
+            CONSTRAINT FOREIGN KEY (entity_id) REFERENCES targets(entity_id)
+            );
+
+            CREATE TABLE IF NOT EXIST attack(
+            attack_id INT PRIMARY KEY,
+            timestamp DATETIME,
+            entity_id VARCHAR(50),
+            weapon_type VARCHAR(50)
+            CONSTRAINT FOREIGN KEY (entity_id) REFERENCES targets(entity_id)
+            );
+
+            CREATE TABLE IF NOT EXIST damage(
+            timestamp DATETIME,
+            attack_id INT,
+            entity_id VARCHAR(50),
+            result VARCHAR(50),
+            CONSTRAINT FOREGIN KEY REFERENCES attack(attack_id)
+            CONSTRAINT FOREIGN KEY (entity_id) REFERENCES targets(entity_id)
+            );
         '''
         cursor.execute(query)
 
